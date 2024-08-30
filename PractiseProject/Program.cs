6f653c0cd36b27669976123861b17af07c911adf
Carl Karama <carlkarama4@gmail.com>
2024-08-30 16:49:06 +1000

    feat: add get request endpoint

--- PractiseProject/Program.cs
+++ PractiseProject/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 /*
     In this file, ASP.NET Core apps are configured. It will launch a host
     responsible for starting the application, configuring an underlying HTTP server
@@ -8,6 +10,13 @@ var builder = WebApplication.CreateBuilder(args); // provides us with API's for 
 var app = builder.Build(); // configures the request-response pipeline behind the scenes. Allows users to configure route handlers in their app
 
 var todos = new List<Todo>();
+
+app.MapGet("/todos", () => todos);
+
+app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) => {
+    var targetTodo = todos.SingleOrDefault(t => id == t.Id);
+    return targetTodo is null ? TypedResults.NotFound() : TypedResults.Ok(targetTodo);
+});
 
 app.MapPost("/todos", (Todo task) => 
 {
