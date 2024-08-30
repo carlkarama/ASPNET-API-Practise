f1d66158033efe0cb742258849e955ed2d142b68
Carl Karama <carlkarama4@gmail.com>
2024-08-30 13:53:56 +1000

    doc: add comments on project startup file

--- PractiseProject/Program.cs
+++ PractiseProject/Program.cs
@@ -1,5 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+/*
+    In this file, ASP.NET Core apps are configured. It will launch a host
+    responsible for starting the application, configuring an underlying HTTP server
+    and setting up the pipeline for processing requests & responses over HTTP. 
+*/
+
+var builder = WebApplication.CreateBuilder(args); // provides us with API's for configuring the application host
+var app = builder.Build(); // configures the request-response pipeline behind the scenes. Allows users to configure route handlers in their app
 
 app.MapGet("/", () => "Hello World!");
 
